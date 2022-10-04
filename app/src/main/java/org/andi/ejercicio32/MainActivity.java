b4399015fcb087441db98efc6cb611f03402ec38
Andrés <tpf21652@educastur.es>
2022-10-04 19:45:30 +0200

    clase 1

--- app/src/main/java/org/andi/ejercicio32/MainActivity.java
+++ app/src/main/java/org/andi/ejercicio32/MainActivity.java
@@ -3,12 +3,21 @@ package org.andi.ejercicio32;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button boton = findViewById(R.id.boton);
+        boton.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
